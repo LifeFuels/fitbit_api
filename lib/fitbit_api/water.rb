e47d736ebaf21d7b5fcef212c553494c03f685ea
Aleks Drozdetski <aleks@lifefuels.co>
2020-04-14 14:46:17 -0400

    Added edit/delete and water goal endpoints

--- lib/fitbit_api/water.rb
+++ lib/fitbit_api/water.rb
@@ -13,5 +13,35 @@ module FitbitAPI
       opts[:unit] = 'ml'
       post("user/#{user_id}/foods/log/water.json", opts)
     end
+
+    def update_water_log(log_id, ml_amount, opts={})
+      opts[:amount] = ml_amount
+      opts[:unit] = 'ml'
+      post("user/#{user_id}/foods/log/water/#{log_id}.json", opts)
+    end
+
+    def update_water_goal(ml_amount, opts={})
+      # First determine the water goal unit
+      res = self.profile
+      return nil if res[:user].nil?
+      unit = res[:user][:water_unit_name]
+      if unit == 'ml'
+        amount = ml_amount
+      elsif unit == 'fl oz'
+        amount = ml_amount * 0.033814
+      else
+        return nil
+      end
+      opts[:target] = amount
+      post("user/#{user_id}/foods/log/water/goal.json", opts)
+    end
+
+    ##
+    # DELETE methods
+
+    # SUCCESS is status = 204
+    def delete_water_log(log_id, opts={})
+      delete("user/#{user_id}/foods/log/water/#{log_id}.json", opts)
+    end
   end
 end
